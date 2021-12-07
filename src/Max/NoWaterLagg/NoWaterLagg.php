ca0ea8bcec6e48ee484c60cd9c080fd899e4b7ba
ItsMax123 <43801744+ItsMax123@users.noreply.github.com>
2021-12-07 16:41:05 -0500

    Add files via upload

--- src/Max/NoWaterLagg/NoWaterLagg.php
+++ src/Max/NoWaterLagg/NoWaterLagg.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Max\NoWaterLagg;
 
+use pocketmine\block\Air;
+use pocketmine\block\StillLava;
+use pocketmine\block\StillWater;
 use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
 use pocketmine\block\Block;
@@ -21,15 +24,22 @@ class NoWaterLagg extends PluginBase implements Listener {
 		$block = $event->getBlock();
 		if ($this->isFlowingBlock($block)) {
 			if ($block->getDamage() == 1) {
-				foreach ($block->getAllSides() as $adjacentBlock) {
-					if ($this->isStillBlock($adjacentBlock)) {
+				if ($this->isStillBlock($block->getLevel()->getBlockAt($block->getX(), $block->getY() + 1, $block->getZ()))) {
+					return;
+				}
+				foreach ($block->getHorizontalSides() as $horizontalSide) {
+					if ($this->isStillBlock($horizontalSide)) {
+						return;
+					}
+					if ($this->isSolidBlock($horizontalSide->getLevel()->getBlockAt($horizontalSide->getX(), $horizontalSide->getY() - 1, $horizontalSide->getZ()))) {
 						return;
 					}
 				}
 				if ($this->isFlowingBlock($block->getLevel()->getBlockAt($block->getX(), $block->getY() - 1, $block->getZ()))) {
 					for ($y = $block->getY(); $y >= 0; $y--) {
-						if ($this->isFlowingBlock($block->getLevel()->getBlockAt($block->getX(), $y, $block->getZ()))) {
-							$block->getLevel()->setBlock(new Vector3($block->getX(), $y, $block->getZ()), Block::get(Block::AIR));
+						$blockY = $block->getLevel()->getBlockAt($block->getX(), $y, $block->getZ());
+						if ($this->isFlowingBlock($blockY)) {
+							$blockY->getLevel()->setBlock($blockY, Block::get(Block::AIR));
 						} else {
 							break;
 						}
@@ -46,8 +56,15 @@ class NoWaterLagg extends PluginBase implements Listener {
 		return False;
 	}
 
-	public function isStillBlock (Block $block) {
-		if (($block instanceof Water or $block instanceof Lava) and ($block->getDamage() == 0 or $block->getDamage() == 8)) {
+	public function isStillBlock(Block $block) {
+		if (($block instanceof Water or $block instanceof Lava) and ($block->getDamage() == 0)) {
+			return True;
+		}
+		return False;
+	}
+
+	public function isSolidBlock(Block $block) {
+		if (!$block instanceof Water and !$block instanceof Lava and !$block instanceof Air){
 			return True;
 		}
 		return False;
