7bb874ef49980423ef5433bd4618c6f543b007a1
ItsMax123 <43801744+ItsMax123@users.noreply.github.com>
2021-12-19 22:28:29 -0500

    Fix bugs

--- src/Max/NoWaterLagg/NoWaterLagg.php
+++ src/Max/NoWaterLagg/NoWaterLagg.php
@@ -1,19 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Max\NoWaterLagg;
 
-use pocketmine\block\Air;
-use pocketmine\block\StillLava;
-use pocketmine\block\StillWater;
-use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
-use pocketmine\block\Block;
-use pocketmine\block\Lava;
-use pocketmine\block\Water;
-use pocketmine\event\block\BlockUpdateEvent;
+use pocketmine\block\{Block, Air, Water};
 use pocketmine\event\Listener;
+use pocketmine\event\block\BlockUpdateEvent;
 
 class NoWaterLagg extends PluginBase implements Listener {
 	public function onEnable() {
@@ -22,26 +14,22 @@ class NoWaterLagg extends PluginBase implements Listener {
 
 	public function onBlockUpdate(BlockUpdateEvent $event) {
 		$block = $event->getBlock();
-		if ($this->isFlowingBlock($block)) {
+		if (($block instanceof Water) and $block->getDamage() != 0 and $block->getDamage() < 8) {
 			if ($block->getDamage() == 1) {
-				if ($this->isStillBlock($block->getLevel()->getBlockAt($block->getX(), $block->getY() + 1, $block->getZ()))) {
-					return;
-				}
-				foreach ($block->getHorizontalSides() as $horizontalSide) {
-					if ($this->isStillBlock($horizontalSide)) {
-						return;
+				if ($block->getLevel()->getBlockAt($block->getX(), $block->getY() + 1, $block->getZ()) instanceof Air) {
+					foreach ($block->getHorizontalSides() as $horizontalSide) {
+						if (($horizontalSide instanceof Water) and ($horizontalSide->getDamage() == 0 or $horizontalSide->getDamage() >= 8)) {
+							return;
+						}
 					}
-					if ($this->isSolidBlock($horizontalSide->getLevel()->getBlockAt($horizontalSide->getX(), $horizontalSide->getY() - 1, $horizontalSide->getZ()))) {
-						return;
-					}
-				}
-				if ($this->isFlowingBlock($block->getLevel()->getBlockAt($block->getX(), $block->getY() - 1, $block->getZ()))) {
-					for ($y = $block->getY(); $y >= 0; $y--) {
-						$blockY = $block->getLevel()->getBlockAt($block->getX(), $y, $block->getZ());
-						if ($this->isFlowingBlock($blockY)) {
-							$blockY->getLevel()->setBlock($blockY, Block::get(Block::AIR));
-						} else {
-							break;
+					if ($this->isNotSource($block->getLevel()->getBlockAt($block->getX(), $block->getY() - 1, $block->getZ()))) {
+						for ($y = $block->getY(); $y >= 0; $y--) {
+							$blockY = $block->getLevel()->getBlockAt($block->getX(), $y, $block->getZ());
+							if ($this->isNotSource($blockY)) {
+								$blockY->getLevel()->setBlock($blockY, Block::get(Block::AIR));
+							} else {
+								break;
+							}
 						}
 					}
 				}
@@ -49,22 +37,8 @@ class NoWaterLagg extends PluginBase implements Listener {
 		}
 	}
 
-	public function isFlowingBlock(Block $block) {
-		if (($block instanceof Water or $block instanceof Lava) and $block->getDamage() != 0 and $block->getDamage() != 8) {
-			return True;
-		}
-		return False;
-	}
-
-	public function isStillBlock(Block $block) {
-		if (($block instanceof Water or $block instanceof Lava) and ($block->getDamage() == 0)) {
-			return True;
-		}
-		return False;
-	}
-
-	public function isSolidBlock(Block $block) {
-		if (!$block instanceof Water and !$block instanceof Lava and !$block instanceof Air){
+	public function isNotSource(Block $block) {
+		if (($block instanceof Water) and $block->getDamage() != 0) {
 			return True;
 		}
 		return False;
